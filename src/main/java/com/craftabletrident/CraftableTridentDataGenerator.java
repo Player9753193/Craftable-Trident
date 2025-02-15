b0f811889c380215a40c50b5091fe8e041a45ca9
Player9753193 <rongxu2023@qq.com>
2025-02-15 17:40:40 +0800

    Create CraftableTridentDataGenerator.java

---
+++ src/main/java/com/craftabletrident/CraftableTridentDataGenerator.java
@@ -0,0 +1,12 @@
+package com.craftabletrident;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.client.MinecraftClient;
+
+public class CraftableTridentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
