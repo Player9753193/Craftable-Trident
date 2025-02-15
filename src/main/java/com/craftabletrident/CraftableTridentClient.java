6fcca118040846babe53cc79c1480285b3e1b4d4
Player9753193 <rongxu2023@qq.com>
2025-02-15 17:37:31 +0800

    Create CraftableTridentClient.java

---
+++ src/main/java/com/craftabletrident/CraftableTridentClient.java
@@ -0,0 +1,10 @@
+package com.craftabletrident;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftableTridentClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
